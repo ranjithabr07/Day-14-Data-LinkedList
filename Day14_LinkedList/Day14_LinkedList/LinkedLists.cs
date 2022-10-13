2928486b281862569c22aaee4fc192e33ab34745
ranjithabr07 <ranjitharanjubrhlk1998@gmail.com>
2022-10-13 12:24:57 +0530

    Sort the elements in Ascending order

---
+++ Day14_LinkedList/Day14_LinkedList/LinkedLists.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Day14_LinkedList
+{
+    public class LinkedLists
+    {
+        public Node? Head;
+        public Node? Tail;
+
+        public LinkedLists()
+        {
+            Head = null;
+            Tail = null;
+        }
+        public void AddNode(int value)
+        {
+            Node node = new Node(value);
+            //Check wheather the list is empty then creates node as head
+            if (Head == null & Tail == null)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                ////If node than add new node as head 
+                node.next = Head;
+                Head = node;
+            }
+            Console.WriteLine($" Added {value} at start of the linkedlist");
+        }
+        public void AppendNode(int value)
+        {
+            Node node = new Node(value);
+            //Check wheather the list is empty then creates node as head
+            if (Head == null & Tail == null)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                Node temp = Head;
+                ////If node than add new node as head 
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine($"Added {value} at start of the linkedlist");
+        }
+        public bool Search(int value)
+        {
+            //Temp variable is head now
+            Node temp = Head;
+            //Check until temp location will null
+            while (temp != null)
+            {
+                //If temp node data is equal to value to be searched
+                if (temp.data == value)
+                {
+                    return true;
+                }
+                temp = temp.next;
+            }
+            return false;
+        }
+        public void InsertNode(int value, int Previousvalue)
+        {
+            Node newNode = new Node(value);
+            if (Head == null & Tail == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Node temp = Head;
+                //Checking until temp variable data equal to the previous value of value to be added
+                while (temp.data != Previousvalue)
+                {
+                    temp = temp.next;
+                }
+                newNode.next = temp.next;
+                temp.next = newNode;
+            }
+        }
+        public void DeleteNodeAtFirst()
+        {
+            if (Head == null & Tail == null)
+            {
+                Console.WriteLine("Nothing to delete list is empty");
+            }
+            Node temp = Head;
+            //Change head to next node to delete the previous node
+            Head = Head.next;
+
+            Console.WriteLine("Removed from linkedlist :" + temp.data);
+        }
+
+        public void DeleteNodeAtLast()
+        {
+
+            if (Head == null & Tail == null)
+            {
+                Console.WriteLine("Nothing to delete list is empty");
+            }
+
+            Node temp = Head;
+            //Check until the temp next nodes's next loction is null
+            while (temp.next.next != null)
+            {
+
+                temp = temp.next;
+            }
+            //After getting this node change its location to null
+            temp.next = null;
+        }
+
+        public int getCount()
+        {
+            Node temp = Head;
+            int count = 0;
+            while (temp != null)
+            {
+                count++;
+                temp = temp.next;
+            }
+            return count;
+
+        }
+        public void deleteNode(int value)
+        {
+            // Store head node
+            Node temp = Head, prev = null;
+
+            // If head node itself holds the key to be deleted
+            if (temp != null && temp.data == value)
+            {
+                // Changed head
+                Head = temp.next;
+                return;
+            }
+
+            // Search for the key to be deleted, keep track of  previous node as we need to change temp.next
+            while (temp != null && temp.data != value)
+            {
+                prev = temp;
+                temp = temp.next;
+            }
+
+            // If key was not present in linked list
+            if (temp == null)
+                return;
+
+            // Unlink the node from linked list
+            prev.next = temp.next;
+        }
+        public void sort()
+        {
+            Node i, j;
+            int temp;
+            for (i = this.Head; i.next != null; i = i.next)
+            {
+                for (j = i.next; j != null; j = j.next)
+                {
+                    if (i.data > j.data)
+                    {
+                        temp = i.data;
+                        i.data = j.data;
+                        j.data = temp;
+                    }
+                }
+            }
+        }
+        internal void Display()
+        {
+            //temp variable will head
+            Node temp = Head;
+            //Check wheather list is empty or not 
+            if (temp == null)
+                Console.WriteLine("Linked list is empty");
+            else
+                Console.WriteLine("Linked List:");
+            while (temp != null)
+            {
+                //Check until temp will null
+                Console.Write(temp.data);
+                if (temp.next != null)
+                    Console.Write(" -> ");
+                //for traversing the node
+                temp = temp.next;
+            }
+
+        }
+    }
+}
---
+++ Day14_LinkedList/Day14_LinkedList/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day14_LinkedList;
+
+Console.WriteLine("Hello, World!");
+
+LinkedLists linkedLists = new LinkedLists();
+
+linkedLists.AppendNode(56);
+linkedLists.AppendNode(30);
+linkedLists.AppendNode(40);
+linkedLists.AppendNode(70);
+linkedLists.sort();
+linkedLists.Display();
